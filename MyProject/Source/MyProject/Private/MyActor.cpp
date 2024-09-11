230bc233911fb485b07ec6c72c936b0bd3616db5
larry <xspanni@gmail.com>
2024-09-11 22:22:16 +0800

    增加一个示例脚本

---
+++ MyProject/Source/MyProject/Private/MyActor.cpp
@@ -0,0 +1,26 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+#include "MyActor.h"
+#include <iostream>
+
+// Sets default values
+AMyActor::AMyActor()
+{
+ 	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+	PrimaryActorTick.bCanEverTick = true;
+	std::cout << "Hello, World!" << std::endl;
+}
+
+// Called when the game starts or when spawned
+void AMyActor::BeginPlay()
+{
+	Super::BeginPlay();
+	
+}
+
+// Called every frame
+void AMyActor::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+
+}
+
